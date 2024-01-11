6cf3b883c5e6c519533efef631973531606146c3
Krati Sengar <kratis011@gmail.com>
2024-01-11 12:59:27 +0530

    m

--- doctest_1.py
+++ doctest_1.py
@@ -17,3 +17,26 @@ def square(x,y):
     125.0
     """
     return int(x**y)
+
+def chocolates(chocolate1, chocolate2, chocolate3):
+  """function to print fav chocolate
+  examples:-
+  >>> chocolates(chocolate1 = "dairymilk", chocolate2 = "bournville", chocolate3 = "truffle")
+  my favourite chocolate is bournville
+  >>> chocolates(chocolate1 = "eclairs", chocolate2 = "amul", chocolate3 = "kitkat")
+  my favourite chocolate is eclairs
+  """
+  print("my favourite chocolate is " + chocolate2)
+
+chocolates(chocolate1 = "dairymilk", chocolate2 = "bournville", chocolate3 = "truffle")
+
+def factorial(n):
+    """function to find factorial
+    example:-
+    >>> factorial(2)
+    2
+    >>> factorial(3)
+    6
+    """
+    return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+
