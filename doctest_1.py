6c4536c3081c2e5b7e76b0e8b3b34c92203ae3f6
Krati Sengar <kratis011@gmail.com>
2024-01-11 12:42:42 +0530

    m

---
+++ doctest_1.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    """function to add two numbers
+    usage examples-
+    >>> add(2,9)
+    11.0
+    >>> add(100.1,9.2)
+    109.3
+    """
+    return float(a + b)
+
+def square(x,y):
+    """function to square a number
+    examples:-
+    >>> square(3,2)
+    9
+    >>> square(5,3)
+    125.0
+    """
+    return int(x**y)
